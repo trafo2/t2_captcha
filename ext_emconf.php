9e05a31e3b60c58c066ccec78b8c10349c8cf0f2
sebastian.noll <sebastian.noll@trafo2.de>
2021-02-15 09:21:06 +0100

    TER release preparations

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = [
 	'title' => 'CAPTCHAs for Extbase forms',
 	'description' => 'reCAPTCHA + hCAPTCHA for Extbase forms',
 	'category' => 'misc',
-	'version' => '1.0.0',
+	'version' => '1.0.1',
 	'state' => 'stable',
 	'uploadfolder' => 0,
 	'clearCacheOnLoad' => 1,
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 	'author_company' => 'Trafo2 GmbH',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '9.4.99-0.0.0',
+			'typo3' => '9.5.0-10.4.99',
 		],
 		'conflicts' => [],
 		'suggests' => [],
